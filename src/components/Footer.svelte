44447f6d951a335cc5033def626a84ef8161d96d
kaiuri <uriel.acioli@gmail.com>
2024-01-11 09:55:30 -0300

    chore: remove unused components

--- src/components/Footer.svelte
+++
@@ -1,4 +0,0 @@
-<div class="flex-row self-center">
-  <em class="self-start">by</em>
-  <a class="self-end" href="https://github.com/kaiuri">me</a>
-</div>
--- src/components/Navigator.svelte
+++
@@ -1,5 +0,0 @@
-<div class="flex flex-row justify-evenly">
-  <slot class="self-start" />
-  <slot class="self-center" />
-  <slot class="self-end" />
-</div>
